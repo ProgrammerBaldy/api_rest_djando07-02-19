bc7a768a5f5b0a8bf3f5d5a4fb8259e53e8672a1
IceShark <tauabn@gmail.com>
2019-02-08 11:38:57 -0200

    heroku

--- settings.py
+++ settings.py
@@ -23,9 +23,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e*w+jln4psj%$+=*yyh+o5ie2)lvj9_n5bc+cp+*8wbe8x$se0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'herokuapp.com', 'herokuapp', 'icesharkrestapi', 'icesharkrestapi.herokuapp.com']
 
 
 # Application definition
