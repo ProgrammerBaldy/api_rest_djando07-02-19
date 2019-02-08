b1aa40164049ff10098c37b02bb2eb8e6810ec62
IceShark <tauabn@gmail.com>
2019-02-08 11:32:16 -0200

    heroku

--- settings.py
+++ settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'e*w+jln4psj%$+=*yyh+o5ie2)lvj9_n5bc+cp+*8wbe8x$se0'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
