e5c52196aeba71d9b08b2f688ee0a47a50fa996f
IceShark <tauabn@gmail.com>
2019-02-12 20:14:27 -0200

    imdNews

--- WebScrappingRestDjango/urls.py
+++ WebScrappingRestDjango/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import url, include
 from django.urls import path  
-from valores_banco.models import valorModel 
+from valores_banco.models import valorModel, newsModel
 from rest_framework import routers, serializers, viewsets
 
 # Serializers define the API representation.
@@ -9,14 +9,22 @@ class valorModelSerializer(serializers.HyperlinkedModelSerializer):
         model = valorModel
         fields = ('valor_banco_brasil', 'valor_banco_bradesco')
 
+class newsModelSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = newsModel
+        fields = ('title', 'text', 'date')
 # ViewSets define the view behavior.
 class valorModelViewSet(viewsets.ModelViewSet):
     queryset = valorModel.objects.all()
     serializer_class = valorModelSerializer
 
+class newsModelViewSet(viewsets.ModelViewSet):
+    queryset = newsModel.objects.all()
+    serializer_class = newsModelSerializer
 # Routers provide an easy way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'valor', valorModelViewSet)
+router.register(r'news', newsModelViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- valores_banco/models.py
+++ valores_banco/models.py
@@ -5,3 +5,8 @@ from django.db import models
 class valorModel(models.Model):
 	valor_banco_brasil = models.BigIntegerField()
 	valor_banco_bradesco = models.BigIntegerField()
+	
+class newsModel(models.Model):
+	title = models.CharField(max_length = 1000)
+	text = models.CharField(max_length = 1000)
+	date = models.CharField(max_length = 30)
